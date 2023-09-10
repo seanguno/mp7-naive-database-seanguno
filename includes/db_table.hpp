9a94b1ad8a75e9989ddf749c485ad4d2366de2e2
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:06:06 -0500

    Update db_table.hpp

--- includes/db_table.hpp
+++ includes/db_table.hpp
@@ -12,26 +12,28 @@ enum class DataType { kString, kDouble, kInt };
 
 class DbTable {
  public:
-  // DbTable() = default;
-  // void AddColumn(const std::pair<std::string, DataType>& col_desc);
-  // void DeleteColumnByIdx(unsigned int col_idx);
+  DbTable() = default;
+  void AddColumn(const std::pair<std::string, DataType>& col_desc);
+  void DeleteColumnByIdx(unsigned int col_idx);
 
-  // void AddRow(const std::initializer_list<std::string>& col_data);
-  // void DeleteRowById(unsigned int id);
+  void AddRow(const std::initializer_list<std::string>& col_data);
+  void DeleteRowById(unsigned int id);
 
-  // DbTable(const DbTable& rhs);
-  // DbTable& operator=(const DbTable& rhs);
-  // ~DbTable();
+  DbTable(const DbTable& rhs);
+  DbTable& operator=(const DbTable& rhs);
+  ~DbTable();
 
-  // friend std::ostream& operator<<(std::ostream& os, const DbTable& table);
+  friend std::ostream& operator<<(std::ostream& os, const DbTable& table);
 
  private:
-  // unsigned int next_unique_id_ = 0;
-  // unsigned int row_col_capacity_ = 2;
-  // std::map<unsigned int, void**> rows_;
-  // std::vector<std::pair<std::string, DataType>> col_descs_;
+  unsigned int next_unique_id_ = 0;
+  unsigned int row_col_capacity_ = 2;
+  std::map<unsigned int, void**> rows_;
+  std::vector<std::pair<std::string, DataType>> col_descs_;
+  void ResizeRow();
+  
 };
 
-// std::ostream& operator<<(std::ostream& os, const DbTable& table);
+std::ostream& operator<<(std::ostream& os, const DbTable& table);
 
 #endif
